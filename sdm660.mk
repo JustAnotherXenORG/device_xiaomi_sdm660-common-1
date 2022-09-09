af711791b038078bc414e07cfe913a5601078b52
xenxynon <flynryder427@gmail.com>
2022-09-09 16:51:02 +0000

    sdm660-common: remove ant radio library
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -119,10 +119,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor \
     antradio_app
 
-# ANT Permission
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # AuthSecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0-service
