d377832d1d145b0cb1d789953111ed9e7c30ec9c
xenxynon <flynryder427@gmail.com>
2022-09-15 07:57:42 +0000

    Revert "sdm660-common: audio: Update audio configs and enable soundtrigger"
    This reverts commit 2328968cef7c6aacb788e8aa8cfa7c4fa25645f3.

--- sdm660.mk
+++ sdm660.mk
@@ -97,7 +97,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(COMMON_PATH)/configs/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(COMMON_PATH)/configs/audio/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml \
-    $(COMMON_PATH)/configs/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
+    $(COMMON_PATH)/configs/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml
 
 # Audio Policy
 PRODUCT_COPY_FILES += \
